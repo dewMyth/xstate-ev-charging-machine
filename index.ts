73260eff8c3e8b00a635256b316232302c04b971
Dewmith Akalanka <akalankadewmith@gmail.com>
2025-01-03 20:23:14 +0530

    add hardcoded events to simulate all state transitions

--- index.ts
+++ index.ts
@@ -15,6 +15,30 @@ const evChargingMachine = createMachine({
       // To test the state transition
       on: {
         START_CHARGING: "Charging",
+        ATTEMPT_AUTHORIZATION: "Authorized",
+      },
+    },
+    Authorized: {
+      entry: assign({
+        message: "Vehicle Authorized! Please plug in your Vehicle to Charge!",
+      }),
+      on: {
+        AUTHORIZATION_SUCCESS: "Starting",
+        AUTHORIZATION_FAILED: "AuthorizationFailed",
+      },
+    },
+    AuthorizationFailed: {
+      entry: assign({
+        message:
+          "Vehicle Authorization Failed! Please try again or Contact Support team!",
+      }),
+    },
+    Starting: {
+      entry: assign({
+        message: "Preparing the Charging Process",
+      }),
+      on: {
+        CHARGING_STARTED: "Charging",
       },
     },
     Charging: {
@@ -23,7 +47,15 @@ const evChargingMachine = createMachine({
       }),
       // After 5 seconds, transition to Idle state automatically
       after: {
-        5000: "Idle",
+        5000: "Stopped",
+      },
+    },
+    Stopped: {
+      entry: assign({
+        message: "Charging Stopped!",
+      }),
+      after: {
+        2000: "Idle",
       },
     },
   },
@@ -41,4 +73,8 @@ evChargingActor.subscribe((state) => {
 evChargingActor.start();
 
 // Simulate user evetn to Change the state from Idle to Charging
-evChargingActor.send({ type: "START_CHARGING" });
+// evChargingActor.send({ type: "START_CHARGING" });
+evChargingActor.send({ type: "ATTEMPT_AUTHORIZATION" });
+// evChargingActor.send({ type: "AUTHORIZATION_FAILED" });
+evChargingActor.send({ type: "AUTHORIZATION_SUCCESS" });
+evChargingActor.send({ type: "CHARGING_STARTED" });
